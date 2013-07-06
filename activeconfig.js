be9c801b47464b3abec6e0447c8d79f0b33fe1ad
Javier Soto <javier.api@gmail.com>
2013-07-06 13:01:53 -0700

    activeconfig.js

--- activeconfig.js
+++ activeconfig.js
@@ -2,8 +2,34 @@ var express = require("express");
 var app = express();
 app.use(express.logger());
 
-app.get('/activeconfig', function(request, response) {
-  response.send('Hello World!');
+app.get('/active_config', function(request, response) {
+	var user_id = request.query.userID || 0
+
+	response.send({
+		meta: {
+			format_version_string: "1.0",
+			creation_time: new Date().toString(),
+		},
+		config_sections: {
+			SampleViewConfiguration: {
+				settings: {
+					ViewBackgroundColor: {
+						value: {
+							red: Math.sin(45 / user_id) / user_id,
+							green: Math.cos(30) / user_id,
+							blue: 1 / user_id,
+						}
+					},
+					LabelText: {
+						value: "This is a label value for user ID " + user_id
+					},
+					ButtonEnabled: {
+						value: (user_id == 3)
+					}
+				}
+			}
+		}
+	})		
 });
 
 var port = process.env.PORT || 5000;
